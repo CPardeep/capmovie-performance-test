6064fda282714d7e4ceb39989870c2d7b8757f54
RGlossop <75027918+RGlossop@users.noreply.github.com>
2021-09-01 08:39:50 +0100

    changed tests to work with users frontend

--- src/test/scala/requests/HomePage.scala
+++ src/test/scala/requests/HomePage.scala
@@ -7,7 +7,7 @@ import io.gatling.http.Predef._
 object HomePage {
   def getHomePage: ChainBuilder = {
     exec(http("Get home page")
-      .get("/home")
+      .get("http://localhost:9001/capmovie/home")
       .check(status.is(200))
     )
   }
